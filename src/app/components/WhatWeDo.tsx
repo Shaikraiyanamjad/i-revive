fa139b6a1f0f18e848b78e49c2a2ef3c1ad98057
Shaikraiyanamjad <raiyanshaik604@gmail.com>
2025-03-07 21:32:34 +0530

    grid changes

--- src/app/components/WhatWeDo.tsx
+++ src/app/components/WhatWeDo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { whatWeDoData } from "../data/whatWeDoData";
 
 const WhatWeDo = () => {
@@ -33,9 +33,16 @@ const WhatWeDo = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, ease: "linear" }}
             viewport={{ once: true }}
-            className="flex flex-col relative group cursor-pointer rounded-lg overflow-hidden shadow-custom-shadow transition-all duration-500 dark:text-black border-b-8 border-primary"
+            className="relative flex flex-col group cursor-pointer rounded-lg overflow-hidden shadow-custom-shadow transition-all duration-500 dark:text-black border-b-8 border-primary"
           >
             <div className="relative h-56 overflow-hidden">
+              <div className="absolute inset-0 bg-primary opacity-0 group-hover:opacity-60 transition-opacity duration-500"></div>
+              <motion.div
+                initial={{ y: "-100%" }}
+                whileHover={{ y: "0%" }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+                className="absolute inset-0 bg-primary opacity-50"
+              ></motion.div>
               <Image
                 src={service.image}
                 alt={service.title}
@@ -46,14 +53,20 @@ const WhatWeDo = () => {
             </div>
 
             <div className="relative bg-white text-black p-6 overflow-hidden flex flex-col flex-1 justify-between">
-              <h3 className="text-xl text-left font-semibold font-serif">
+              <motion.div
+                initial={{ y: "100%" }}
+                whileHover={{ y: "0%" }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+                className="absolute inset-0 bg-primary opacity-50"
+              ></motion.div>
+              <h3 className="text-xl text-left font-semibold font-serif relative z-10">
                 {service.title}
               </h3>
-              <p className="mt-3 text-base text-left flex-1">
+              <p className="mt-3 text-base text-left flex-1 relative z-10">
                 {service.description}
               </p>
               <button
-                className="mt-4 bg-primary text-white py-3 px-5 rounded-full hover:bg-white hover:text-hovershed border hover:border-hovershed transition"
+                className="mt-4 bg-primary text-white py-3 px-5 rounded-full hover:bg-white hover:text-hovershed border hover:border-hovershed transition relative z-10"
               >
                 Read More
               </button>
